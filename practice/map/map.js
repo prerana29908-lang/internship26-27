b78bdb5fff2ec607105703011190929e2dbca5aa
prerana29908-lang <prerana29908@gmail.com>
2026-06-21 20:24:53 +0530

    first commit

--- practice/map/map.js
+++ practice/map/map.js
@@ -1,6 +1,28 @@
-const user = new Map();
+// const user = new Map();
 
-user.set("name", "Prerana");
-user.set("age", 20);
+// user.set("name", "Prerana");
+// user.set("age", 20);
 
-console.log(user);
+// console.log(user);
+
+// A Map is a special object used to store key-value pairs.
+
+// Unlike a normal object, a Map can use any data type as a 
+// key (string, number, object, function, etc.).
+
+// console.log(user.get("name"));
+// console.log(user.has("age"));//true
+
+// user.delete("age");
+
+// console.log(user.size);
+
+//Optional chaining
+// const user = {
+//     name: "Prerana",
+//     address: {
+//         city: "Mumbai"
+//     }
+// };
+
+// console.log(user.address?.city);
