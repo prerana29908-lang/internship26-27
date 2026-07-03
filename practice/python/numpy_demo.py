7a3f1fc803b33faf0ac200d79342427a2d0b6bf9
Prerana29908-lang <prerana29908@gmail.com>
2026-07-03 10:46:54 -0700

    practice

---
+++ practice/python/numpy_demo.py
@@ -0,0 +1,30 @@
+# NumPy (short for Numerical Python) is a
+# Python library used for fast numerical computing.
+# It provides a powerful multidimensional array object and
+# many functions for performing mathematical
+# operations on arrays efficiently.
+
+import numpy as np
+# arr1=np.array([1,2,3,4,5])
+# print(arr1);
+
+# arr2=np.array([[1,2,3],[4,5,6]]);
+# print(arr2) 
+# //np.array means call the numpy function from numpy library
+
+# arr3=np.array([[[1,2],[3,4]],[[5,6],[7,8]]])
+# print(arr3)
+
+# zeros=np.zeros((3,4))
+# print(zeros)
+
+# ones=np.ones((2,3,4),dtype=np.int16)
+# print(ones)
+# //2 blocks,3 rows,4cols
+
+# empty=np.empty((2,3))
+# print(empty)
+# //garbage values
+
+identify =np.eye(3)
+print(identify)
